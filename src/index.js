0bb60342381536aa6baa7de0d004572a7bf488fb
Altruist2692 <altruist2692@gmail.com>
2020-02-02 22:41:14 +0530

    initial commit with sample scrapper

---
+++ src/index.js
@@ -0,0 +1,93 @@
+const setup = require('./starter-kit/setup');
+
+exports.handler = async (event, context, callback) => {
+  // For keeping the browser launch
+  context.callbackWaitsForEmptyEventLoop = false;
+  const browser = await setup.getBrowser();
+  try {
+    const result = await exports.run(browser);
+    callback(null, result);
+  } catch (e) {
+    callback(e);
+  }
+};
+
+exports.run = async (browser) => {
+  // implement here
+  // this is sample
+  const page = await browser.newPage();
+  await page.goto('https://www.thestorefront.com/spaces/united-states/new-york/new-york/28331-boutique-in-trendy-nolita',
+  );
+  // console.log((await page.content()).slice(0, 500));
+
+  // await page.type('#lst-ib', 'aaaaa');
+  // avoid to timeout waitForNavigation() after click()
+  await Promise.all([
+    // avoid to
+    // 'Cannot find context with specified id undefined' for localStorage
+    page.waitForSelector('.title'),
+    // page.click('[name=btnK]'),
+  ]);
+
+/* screenshot
+  await page.screenshot({path: '/tmp/screenshot.png'});
+  const aws = require('aws-sdk');
+  const s3 = new aws.S3({apiVersion: '2006-03-01'});
+  const fs = require('fs');
+  const screenshot = await new Promise((resolve, reject) => {
+    fs.readFile('/tmp/screenshot.png', (err, data) => {
+      if (err) return reject(err);
+      resolve(data);
+    });
+  });
+  await s3.putObject({
+    Bucket: '<bucket name>',
+    Key: 'screenshot.png',
+    Body: screenshot,
+  }).promise();
+*/
+  // console.log()
+  // cookie and localStorage
+  // await page.setCookie({name: 'name', value: 'cookieValue'});
+  // console.log(await page.cookies());
+  // console.log(await page.evaluate(() => {
+  //   localStorage.setItem('name', 'localStorageValue');
+  //   return localStorage.getItem('name');
+  // }));
+
+  //title
+  const title = await page.$eval('h1.title', element => element.textContent);
+  console.log(title)
+
+  const pricingValue = await page.$eval('span.pricing-value', element => element.textContent);
+  console.log('Pricing Value: ' + pricingValue)
+
+  const location = await page.$eval('h2.location', element => element.textContent);
+  console.log('Location' + location)
+
+  const infoTitle= await page.evaluate(() => Array.from(document.querySelectorAll('div.listing-details-panel .listing-information .title'), element => element.textContent));
+  const infoValue = await page.evaluate(() => Array.from(document.querySelectorAll('div.listing-details-panel .listing-information .information'), element => element.textContent));
+  const info = {}
+  for (var i = 0; i < infoTitle.length; i++) {
+    info[infoTitle[i]] = infoValue[i]
+  }
+  console.log(info)
+
+  const description = await page.$eval('div.listing-description', element => element.textContent);
+  console.log('description: ' + description)
+
+  const spaceUsage= await page.evaluate(() => Array.from(document.querySelectorAll('.listing-details-panel.project_types .parent-project-type'), element => element.textContent));
+  console.log('spaceUsage: ' + spaceUsage)
+
+  const amenities = await page.evaluate(() => Array.from(document.querySelectorAll('.listing-information .listing-features'), element => element.textContent));
+  console.log('' + amenities)
+
+  const lastUpdatedAt = await page.$eval('span.last-updated-date', element => element.textContent);
+  console.log('lastUpdatedAt: ' + lastUpdatedAt)
+
+  const imgs = await page.evaluate(() => Array.from(document.querySelectorAll('div.sf-carousel.ng-isolate-scope .image'), (element) => { return getComputedStyle(element).backgroundImage.replace('url\(', '').replace(/\"/g, '').replace('\)', '') }));
+  console.log(imgs)
+  
+  await page.close();
+  return 'done';
+};
